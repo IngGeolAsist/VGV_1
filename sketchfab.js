f937f4a8b33963d67350815f35af101d20e1b7e8
IngGeolAsist <80356680+IngGeolAsist@users.noreply.github.com>
2021-08-09 21:56:03 -0500

    Update and rename Sitios.js to sketchfab.js

---
+++ sketchfab.js
@@ -0,0 +1,8 @@
+var sketchfab = {
+"type": "FeatureCollection",
+"name": "sketchfab",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "La Cima II", "id": 2 }, "geometry": { "type": "Point", "coordinates": [ -99.195031329585177, 19.111837653820984 ] } },
+]
+};
