825712c448d5f8ec3f1e872fcb2ae0d555e4be5c
IngGeolAsist <80356680+IngGeolAsist@users.noreply.github.com>
2021-08-09 22:09:06 -0500

    Update sketchfab.js

--- sketchfab.js
+++ sketchfab.js
@@ -3,6 +3,6 @@ var sketchfab = {
 "name": "sketchfab",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "Name": "La Cima II", "id": 2 }, "geometry": { "type": "Point", "coordinates": [ -99.195031329585177, 19.111837653820984 ] } },
+{ "type": "Feature", "properties": { "Name": "La Cima II", "id": 2 }, "geometry": { "type": "Point", "coordinates": [ -99.195205, 19.113425] } },
 ]
 };
